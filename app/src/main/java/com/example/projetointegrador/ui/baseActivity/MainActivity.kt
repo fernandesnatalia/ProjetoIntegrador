bff1c2e52c401c881a65291c02ad939b10dc5074
Natália Fernandes <natalia1fernandes1@gmail.com>
2022-09-28 17:10:02 -0300

    navhost in activity

--- app/src/main/java/com/example/projetointegrador/ui/baseActivity/MainActivity.kt
+++ app/src/main/java/com/example/projetointegrador/ui/baseActivity/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.projetointegrador.ui.baseActivity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.fragment.NavHostFragment
 import com.example.projetointegrador.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -11,5 +12,8 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        supportFragmentManager.findFragmentById(binding.navHostFragment.id) as NavHostFragment
+
     }
 }
