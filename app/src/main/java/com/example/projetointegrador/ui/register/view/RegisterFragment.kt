0c803fca565a08a103279e1b7bb2f96e4b422f1b
Natália Fernandes <natalia1fernandes1@gmail.com>
2022-09-28 20:21:51 -0300

    register flow

---
+++ app/src/main/java/com/example/projetointegrador/ui/register/view/RegisterFragment.kt
@@ -0,0 +1,23 @@
+package com.example.projetointegrador.ui.register.view
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.projetointegrador.databinding.FragmentRegisterBinding
+
+class RegisterFragment : Fragment() {
+    private lateinit var binding: FragmentRegisterBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = FragmentRegisterBinding.inflate(layoutInflater, container, false)
+        return binding.root
+    }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
